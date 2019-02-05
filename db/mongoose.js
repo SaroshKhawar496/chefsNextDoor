ef61d10c5401a18e30917309c009fe7cffd863b2
Sarosh <43619748+SaroshKhawar496@users.noreply.github.com>
2019-02-05 00:34:10 -0500

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 // connect to our database
-mongoose.connect('mongodb://localhost:27017/StudentAPI', { useNewUrlParser: true});
+mongoose.connect('mongodb://sarosh-chef-next-door:sushi595@ds121105.mlab.com:21105/chefs_next_door_db', { useNewUrlParser: true});
 
 module.exports = { mongoose }
