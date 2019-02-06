bd51cc8ea8a9a92bd8f6ddf2add67f16afdce3e5
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-06 13:23:28 -0500

    Using .env file now

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose')
 
 // connect to our database
-mongoose.connect('mongodb://sarosh-chef-next-door:sushi595@ds121105.mlab.com:21105/chefs_next_door_db', { useNewUrlParser: true});
+mongoose.connect('mongodb://'+process.env.DB_USER_NAME+':'+process.env.DB_PWD+'@ds121105.mlab.com:21105/'+process.env.DB_NAME, { useNewUrlParser: true});
 
 module.exports = { mongoose }
